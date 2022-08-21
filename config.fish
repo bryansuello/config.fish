2c63a3b379b63d1f213d1f0c8c2a45304798d5bf
brys <brys@weapon.localdomain>
2022-08-21 12:08:04 +0800

    add tmux remove and install alias 'tmuxfix'

--- config.fish
+++ config.fish
@@ -1,8 +1,12 @@
-
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Commands to run in interactive sessions can go here
+
+# Set starship as prompt
+starship init fish | source
+
 
 set -U fish_greeting ""
+
 
 #alias ls="ls -lah --color=auto"
 alias ls="exa -lah --icons --group-directories-first"
@@ -11,6 +15,7 @@ alias cl="clear"
 alias tm="tmux"
 alias 1="cd /home/brys/dev/"
 alias 2="cd /home/brys/.config/"
+alias tmuxfix="sudo zypper remove tmux -y && sudo zypper in tmux -y" #tmux fix issue after restart. uninstall and install
 
 end
 
