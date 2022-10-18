cd3b408b7036bed4b90271c47a48ae67f65d1be0
Suello, Bryan <bryansuello@gmail.com>
2022-10-18 11:57:35 +0800

    bat update

--- config.fish
+++ config.fish
@@ -10,13 +10,13 @@ set -U fish_greeting ""
 
 #alias ls="ls -lah --color=auto"
 alias ls="exa -lah --icons --group-directories-first"
-alias cat="bat"
+alias cat="batcat"  #instead of bat
 alias v="nvim"
 alias vim="nvim"
 alias cl="clear"
 alias tm="tmux"
-alias 1="cd /home/brys/dev/"
-alias 2="cd /home/brys/.config/"
+alias 1="cd ~/brys/dev/"
+alias 2="cd ~/.config/"
 alias tmuxfix="sudo zypper remove tmux -y && sudo zypper in tmux -y" #tmux fix issue after restart. uninstall and install
 alias n="node"
 alias t="tree"
@@ -28,4 +28,3 @@ alias play="node ~/dev/notes/terminalQuizG/app.js"
 export HOST=localhost #npm start error fix, opens in weapon:3000 instead of localhost:3000/
 
 end
-
