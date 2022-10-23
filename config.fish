f1ee6ad6cc5085ffa2a80380506be7e52fbd4b8d
Bryan Suello <brys@weapon.localdomain>
2022-10-24 00:26:16 +0800

    feat: add win dir alias, obsidian

--- config.fish
+++ config.fish
@@ -11,17 +11,19 @@ set -U fish_greeting ""
 #alias ls="ls -lah --color=auto"
 alias ls="exa -lah --icons --group-directories-first"
 alias cat="batcat"  #instead of bat
-alias v="nvim"
-alias vim="nvim"
+alias vim="lvim"
 alias cl="clear"
 alias tm="tmux"
-alias 1="cd ~/brys/dev/"
+alias 1="cd /home/brys/dev/"
 alias 2="cd ~/.config/"
   #alias tmuxfix="sudo zypper remove tmux -y && sudo zypper in tmux -y" #tmux fix issue after restart. uninstall and install
 alias n="node"
 alias t="tree"
 alias emacs="code"
 alias b="bash"
+alias obsidian="cd /mnt/c/Obsidian_Win/"
+alias downloads="cd /mnt/c/Users/brys/Downloads/"
+alias ssd="cd /mnt/x/Downloads\ \(SSD\)/"
 
 
 alias play="node ~/dev/notes/terminalQuizG/app.js"
@@ -31,5 +33,3 @@ alias play="node ~/dev/notes/terminalQuizG/app.js"
 export HOST=localhost 
 
 end
-
-
