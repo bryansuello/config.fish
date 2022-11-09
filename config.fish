7416132bdce58dae5f54adbcbe9e8ba0ad067a4c
bryansuello <bryansuello@gmail.com>
2022-11-09 15:36:30 +0800

    feat: add fzf config(batcat file previewm, colorscheme)

--- config.fish
+++ config.fish
@@ -39,4 +39,13 @@ export HOST=localhost
 # Set starship as prompt
 #starship init fish | source
 
+# fzf custom - file preview, nord colorscheme
+export FZF_DEFAULT_OPTS="
+--preview 'batcat --style numbers,changes --theme Nord --color=always  {}'
+--border rounded
+--color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+--color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
+"
+
+
 end
